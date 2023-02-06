535ada6515f8ffbbe1ee2ef51bc53c966523489b
Amlal <113760121+amllx@users.noreply.github.com>
2023-02-06 09:03:45 +0100

    Update MIPSIntrusctionFactory.d
    Fix nonsense.
    Also added a seek function.

--- libmips/MIPSIntrusctionFactory.d
+++ libmips/MIPSIntrusctionFactory.d
@@ -11,43 +11,33 @@ final class MIPSIntrusctionFactory {
 		this.Fp = File(OutName ~ ".mips.xar", "wb");
 	}
 
-	ulong make_imm_opcode(int op, int rs, int rt, int imm) {
+	void seek(ulong off) { this.Fp.seek(off); }
+
+	void make_imm_opcode(int op, int rs, int rt, int imm) {
 		this.Fp.write(op);
 		this.Fp.write(rs);
 		this.Fp.write(rt);
 		this.Fp.write(imm);
-
-		ulong ret = op | rs | rt | imm;
-		return ret;
 	}
 
-	ulong make_reg_opcode(int op, int rs, int rt, int low, int hi) {
+	void make_reg_opcode(int op, int rs, int rt, int low, int hi) {
 		this.Fp.write(op);
 		this.Fp.write(rs);
 		this.Fp.write(rt);
 		this.Fp.write(low);
 		this.Fp.write(hi);
-		
-		ulong ret = op | rs | rt | low | hi;
-		return ret;
 	}
 
-	ulong make_branch_opcode(int op, int rs, int branch, int off) {
+	void make_branch_opcode(int op, int rs, int branch, int off) {
 		this.Fp.write(op);
 		this.Fp.write(rs);
 		this.Fp.write(branch);
 		this.Fp.write(off);
-
-		ulong ret = op | rs | branch | off;
-		return ret;
 	}
 
-	ulong make_syscall_opcode(int op, int ivt, int isr) {
+	void make_syscall_opcode(int op, int ivt, int isr) {
 		this.Fp.write(op);
 		this.Fp.write(ivt);
 		this.Fp.write(isr);
-
-		ulong ret = op | ivt | isr;
-		return ret;
 	}
 }
