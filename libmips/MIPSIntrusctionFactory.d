d9b5b5ca768744ac989ef889ae3a0666a31d1f68
Amlal <113760121+amllx@users.noreply.github.com>
2023-02-06 09:15:52 +0100

    make_*, improve write calls. It was like calling each time a system call.
    Signed-off-by: Amlal <113760121+amllx@users.noreply.github.com>

--- libmips/MIPSIntrusctionFactory.d
+++ libmips/MIPSIntrusctionFactory.d
@@ -11,33 +11,23 @@ final class MIPSIntrusctionFactory {
 		this.Fp = File(OutName ~ ".mips.xar", "wb");
 	}
 
-	void seek(ulong off) { this.Fp.seek(off); }
+	void seek(ulong off) { 
+		this.Fp.seek(off); 
+	}
 
 	void make_imm_opcode(int op, int rs, int rt, int imm) {
-		this.Fp.write(op);
-		this.Fp.write(rs);
-		this.Fp.write(rt);
-		this.Fp.write(imm);
+		this.Fp.write([op, rs, rt, imm]);
 	}
 
 	void make_reg_opcode(int op, int rs, int rt, int low, int hi) {
-		this.Fp.write(op);
-		this.Fp.write(rs);
-		this.Fp.write(rt);
-		this.Fp.write(low);
-		this.Fp.write(hi);
+		this.Fp.write([op, rs, rt, low, hi]);
 	}
 
 	void make_branch_opcode(int op, int rs, int branch, int off) {
-		this.Fp.write(op);
-		this.Fp.write(rs);
-		this.Fp.write(branch);
-		this.Fp.write(off);
+		this.Fp.write([op, rs, branch, off]);
 	}
 
 	void make_syscall_opcode(int op, int ivt, int isr) {
-		this.Fp.write(op);
-		this.Fp.write(ivt);
-		this.Fp.write(isr);
+		this.Fp.write([op, ivt, isr]);
 	}
 }
